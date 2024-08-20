38dba03d4b1b4036196fa8ec65397b97db9daefc
ZUBOV ILLIA <zubovillia@gmail.com>
2024-08-20 11:13:35 +0300

    removed commented code

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 // const path = require('path');
-// const { error } = require('console');
 const app = express();
 const port = 3001;
 
